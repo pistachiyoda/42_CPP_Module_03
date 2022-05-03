858d803fa3a29030a551cd9e305ed56db5e3da2d
mai <fujisawamai21@gmail.com>
2022-05-03 22:35:50 +0900

    ex00をそのままコピー

---
+++ ex01/main.cpp
@@ -0,0 +1,59 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap a("A");
+    ClapTrap copied_a(a);
+    ClapTrap c("C");
+    ClapTrap substituted_c = c;
+    std::cout << std::endl;
+
+    std::cout << "~~ Fight of ClapTrap A ~~" << std::endl;
+    a.attack("Alice");
+    a.takeDamage(5);
+    a.beRepaired(3);
+    a.takeDamage(8);
+    a.attack("Alice");
+    a.takeDamage(8);
+    std::cout << std::endl;
+
+    std::cout << "~~ Fight of ClapTrap copied_a ~~" << std::endl;
+    copied_a.attack("Bob");
+    copied_a.takeDamage(9);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    copied_a.beRepaired(2);
+    std::cout << std::endl;
+
+    std::cout << "~~ Fight of ClapTrap C ~~" << std::endl;
+    c.attack("Alice");
+    c.takeDamage(5);
+    c.beRepaired(3);
+    c.takeDamage(8);
+    c.attack("Alice");
+    c.takeDamage(8);
+    std::cout << std::endl;
+
+    std::cout << "~~ Fight of ClapTrap substituted_c ~~" << std::endl;
+    substituted_c.attack("Bob");
+    substituted_c.takeDamage(9);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    substituted_c.beRepaired(2);
+    std::cout << std::endl;
+    return 0;
+}
