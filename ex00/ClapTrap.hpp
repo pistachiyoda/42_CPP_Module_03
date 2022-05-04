70a85ec8de4901317fbc2552fe2a24bc2f0554d5
mai <fujisawamai21@gmail.com>
2022-05-04 21:53:44 +0900

    ex00の出力を分かりやすいように修正

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -12,16 +12,16 @@ private:
     unsigned int energyPoints_;
     unsigned int attackDamage_;
     bool isActive();
-    void showStatus();
 public:
     ClapTrap(std::string name);
     ~ClapTrap();
     ClapTrap(const ClapTrap &claptrap);
     ClapTrap &operator=(const ClapTrap &claptrap);
     std::string getName() const;
+    void showStatus();
     unsigned int getHitPoint() const;
     unsigned int getEnergyPoint() const;
-    unsigned int getAttackDamage() const; 
+    unsigned int getAttackDamage() const;
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,18 @@
 
 int main()
 {
+    std::cout << "~~ Generate ClapTrap A ~~" << std::endl;
     ClapTrap a("A");
+    std::cout << std::endl;
+    std::cout << "~~ Generate copied ClapTrap A by copy constructor ~~" << std::endl;
     ClapTrap copied_a(a);
+    std::cout << std::endl;
+    std::cout << "~~ Generate ClapTrap C ~~" << std::endl;
     ClapTrap c("C");
-    ClapTrap substituted_c = c;
     std::cout << std::endl;
 
     std::cout << "~~ Fight of ClapTrap A ~~" << std::endl;
+    a.showStatus();
     a.attack("Alice");
     a.takeDamage(5);
     a.beRepaired(3);
@@ -18,6 +23,7 @@ int main()
     std::cout << std::endl;
 
     std::cout << "~~ Fight of ClapTrap copied_a ~~" << std::endl;
+    copied_a.showStatus();
     copied_a.attack("Bob");
     copied_a.takeDamage(9);
     copied_a.beRepaired(2);
@@ -33,15 +39,20 @@ int main()
     std::cout << std::endl;
 
     std::cout << "~~ Fight of ClapTrap C ~~" << std::endl;
+    c.showStatus();
     c.attack("Alice");
     c.takeDamage(5);
     c.beRepaired(3);
-    c.takeDamage(8);
-    c.attack("Alice");
-    c.takeDamage(8);
+    c.showStatus();
+    std::cout << std::endl;
+
+    std::cout << "~~ Generate copied ClapTrap C by = operator. ~~" << std::endl;
+    ClapTrap substituted_c("X");
+    substituted_c = c;
     std::cout << std::endl;
 
     std::cout << "~~ Fight of ClapTrap substituted_c ~~" << std::endl;
+    substituted_c.showStatus();
     substituted_c.attack("Bob");
     substituted_c.takeDamage(9);
     substituted_c.beRepaired(2);
